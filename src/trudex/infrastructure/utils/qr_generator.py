1a8027167cb2177665e8c9d2a5e0b36f3772b86a
kolo <kolo.is.main@gmail.com>
2026-01-03 03:19:35 +0300

    Initial commit

--- src/trudex/infrastructure/utils/qr_generator.py
+++ src/trudex/infrastructure/utils/qr_generator.py
@@ -4,7 +4,6 @@ import qrcode
 
 
 def generate_qr_bytes(text: str) -> bytes:
-    """Generate QR code as PNG bytes."""
     img = qrcode.make(text)
     with io.BytesIO() as buffer:
         img.save(buffer)
